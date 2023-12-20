cc4c9b9b09022388bb461aae3d7999a6b3002938
Jason Sturges <jason@jasonsturges.com>
2023-12-20 14:15:38 -0600

    Three.js scene

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,34 @@
-import './style.css'
+import "./style.css";
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+
+// Create a scene
+const scene = new Scene();
+
+// Create a camera
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.z = 5;
+
+// Create a renderer
+const renderer = new WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x090909);
+document.body.appendChild(renderer.domElement);
+
+// Animation loop
+renderer.setAnimationLoop(() => {
+  renderer.render(scene, camera);
+});
+
+// Resize handler
+const resize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
+window.addEventListener("resize", resize);
